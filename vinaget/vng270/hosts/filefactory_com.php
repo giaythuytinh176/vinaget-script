17880e97fd300b8f08a45b15b0db9bca515609a7
giaythuytinh176@gmail.com <giaythuytinh176@gmail.com@5571b426-2644-ecfe-927b-6b2e40b3eb12>
2013-08-10 19:19:03 +0000

    Fix download link Filefactory, Add support file password

--- vinaget/vng270/hosts/filefactory_com.php
+++ vinaget/vng270/hosts/filefactory_com.php
@@ -3,46 +3,59 @@
 class dl_filefactory_com extends Download {
 
 	public function CheckAcc($cookie){
-		$data = $this->lib->curl("http://www.filefactory.com/premium/", $cookie, "");
+		$data = $this->lib->curl("http://www.filefactory.com/premium/", "ff_locale=en_US.utf8;".$cookie, "");
 		if(stristr($data, 'Premium member until')) return array(true, "Until ".$this->lib->cut_str($data, '<p><a href="/premium/">','. Extend</a></p>'));
 		else if(stristr($data, '<p class="greenText">Free member</p>')) return array(false, "accfree");
 		else return array(false, "accinvalid");
 	}
-
+	
 	public function Login($user, $pass){
 		$post["email"] = $user;
 		$post["password"] = $pass;
 		$post['redirect'] = "http://www.filefactory.com/";
-		$data = $this->lib->curl("http://www.filefactory.com/","",$post);
-		$cookie = $this->lib->GetCookies($data);
+		$data = $this->lib->curl("http://www.filefactory.com/", "ff_locale=en_US.utf8", $post);
+		$cookie = "ff_locale=en_US.utf8;".$this->lib->GetCookies($data);
 		return $cookie;
 	}
 	
     public function Leech($url) {
-		if(!stristr($url, "www")) {
-			$ex = explode("filefactory.com", $url);
-			$url = "http://www.filefactory.com".$ex[1];
+		if(!stristr($url, "/n/")) {
+			$ex =  explode("/", $url); 
+			$url = "http://www.filefactory.com/file/".$ex[4]."/n/".$ex[5];
 		}
+		list($url, $pass) = $this->linkpassword($url);
 		$data = $this->lib->curl($url,$this->lib->cookie,"");
+		if($pass) {
+			$post["password"] = $pass;
+			$post["action"] = "password";
+			$data = $this->lib->curl($url, $this->lib->cookie, $post);
+			if(stristr($data,'Incorrect password.')) 	$this->error("reportpass", true, false);
+			elseif($this->isredirect($data)) return trim($this->redirect);
+			elseif(preg_match('%(http:\/\/.+filefactory\.com/dlp/.+)">Download with F%U', $data, $redir2)) 
+			return trim($redir2[1]);
+		}
 		if($this->isredirect($data)) return trim($this->redirect);
-		elseif(preg_match('%(http:\/\/.+filefactory\.com/dlp/.+)">Download with FileFactory Premium%U', $data, $redir2)) $link = trim($redir2[1]);
+		elseif(preg_match('%(http:\/\/.+filefactory\.com/dlp/.+)">Download with F%U', $data, $redir2)) 
+		return trim($redir2[1]);
 		if(!empty($link)){
 			if(!stristr($link, "logout.php")) return $link;
 			else $this->error("notwork");
 		}
+		elseif(stristr($data,'You are trying to access a password protected file')) 	$this->error("reportpass", true, false);
 		elseif (stristr($data,"This error is usually caused by requesting a file that does not exist")) $this->error("dead", true, false, 2);
 		return false;
     }
-
+	
 }
 
 /*
 * Open Source Project
 * Vinaget by ..::[H]::..
 * Version: 2.7.0
-* Filefactory.com Download Plugin by .:[T]:.
+* Filefactory.com Download Plugin by giaythuytinh176
 * Downloader Class By [FZ]
 * Date: 16.7.2013
 * Fixed check account: 18.7 
+* Fix download link Filefactory, Add support file password by giaythuytinh176 [29.7.2013]
 */
 ?>
