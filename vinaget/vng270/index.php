612132cb44a5eef372bcf0b4232365572873a11c
giaythuytinh176@gmail.com <giaythuytinh176@gmail.com@5571b426-2644-ecfe-927b-6b2e40b3eb12>
2013-08-25 16:48:09 +0000

    use like config.php work with goo.gl, adf.ly, bc.vc

--- vinaget/vng270/index.php
+++ vinaget/vng270/index.php
@@ -24,7 +24,7 @@ define('vinaget', 'yes');
 include("class.php");
 $obj = new stream_get(); 
 $obj->using = $using;
-$obj->current_version = 50;
+$obj->current_version = 51;
 $obj->msg = false;
 if(isset($_COOKIE['msg'])) {
 	$obj->msg = $_COOKIE['msg'];
