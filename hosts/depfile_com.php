98aaa6f0f59b7cb3eea056fb9e98659fcb43153d
hogeunk <meteo,minaduki@gmail.com>
2016-02-19 17:22:30 +0900

    Fix depfile_com.php by Naztek
    Fixed by Naztek - 26.11.2015
    [Naztek.tk]

--- hosts/depfile_com.php
+++ hosts/depfile_com.php
@@ -1,30 +1,32 @@
-<?php	
+<?php   
 
 class dl_depfile_com extends Download {
-    
-    public function CheckAcc($cookie){
-        $data = $this->lib->curl("https://depfile.com/", "sdlanguageid=2;{$cookie}", "");
-        if(stristr($data, '>Premium: <a class=')) return array(true, "Until ".$this->lib->cut_str($data, "premium'>", '</a></div>'));
+
+	public function CheckAcc($cookie){
+		$data = $this->lib->curl("https://depfile.com/", "sdlanguageid=2;{$cookie}", "");
+		if(stristr($data, '<div class=exit>')) return array(true, "Until ".$this->lib->cut_str($data, "space/premium'>", '<img src='));
 		else return array(false, "accinvalid");
-    }
-    
-    public function Login($user, $pass){
-        $data = $this->lib->curl("https://depfile.com/", "sdlanguageid=2", "login=login&loginemail={$user}&loginpassword={$pass}&submit=login&rememberme=1");
-        $cookie = "sdlanguageid=2;{$this->lib->GetCookies($data)}";
-        return $cookie;
-    }
-     
-    public function Leech($url) {
+	}
+
+	public function Login($user, $pass){
+		$data = $this->lib->curl("https://depfile.com/", "sdlanguageid=2", "login=login&loginemail={$user}&loginpassword={$pass}&submit=login&rememberme=on");
+		$cookie = "sdlanguageid=2;{$this->lib->GetCookies($data)}";
+		return $cookie;
+	}
+
+	public function Leech($url) {
 		if(!stristr($url, "https")) {
 			$url = str_replace('http', 'https', $url);
 		}
 		$data = $this->lib->curl($url, $this->lib->cookie, "");
-        if(stristr($data,'Page Not Found!') || stristr($data,'File was not found in the') || stristr($data,'Provided link contains errors')) $this->error("dead", true, false, 2);
-		elseif(preg_match('@https?:\/\/[a-z]+\.depfile\.com\/premdw\/\d+\/[a-z0-9]+\/[^"\'<>\r\n\t]+@i', $data, $giay)) 
-		return trim($giay[0]);
+		if(stristr($data,'Page Not Found!') || stristr($data,'File was not found in the') || stristr($data,'Provided link contains errors')) {
+			$this->error("dead", true, false, 2);
+		} elseif(preg_match('@https?:\/\/[a-z]+\.depcloud\.com\/premdw\/\d+\/[a-z0-9]+\/[^"\'<>\r\n\t]+@i', $data, $giay)) {
+			return trim($giay[0]);
+		}
 		return false;
-    }
-	
+	}
+
 }
 
 /*
@@ -34,5 +36,7 @@ class dl_depfile_com extends Download {
 * depfile.com Download Plugin
 * Downloader Class By [FZ]
 * Download plugin by giaythuytinh176 [11.8.2013]
+* Fixed by Naztek - 26.11.2015
+* [Naztek.tk]
 */
 ?>
