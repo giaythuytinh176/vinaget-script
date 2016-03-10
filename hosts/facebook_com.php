4382c679c82f978c3474091907170a787b8234fd
hogeunk <meteo,minaduki@gmail.com>
2016-03-10 14:38:15 +0900

    Fix facebook_com.php

--- hosts/facebook_com.php
+++ hosts/facebook_com.php
@@ -1,18 +1,21 @@
 <?php
 
 class dl_facebook_com extends Download {
-	
-    public function FreeLeech($url) {
+
+	public function FreeLeech($url) {
 		$data = $this->lib->curl($url, '', '');
-		$temp = $this->lib->cut_str($data, '"videoData":[', ']');
-		$json = @json_decode($temp, true);
+		$source = ["\u00253A", "\u00252F", "\u00253F", "\u00253D", "\u002526", "\u00257B", "\u00257D", "\u002522", "\u00252C", "\u00255C", "\u00255D", "\u00255B", "\\"];
+		$replace = [":", "/", "?", "=", "&", "{", "}", "\"", ",", "\\", "[", "]", ""];
+		$data = str_replace($source, $replace, $data);
 		$qualities = ["hd_src_no_ratelimit", "hd_src", "sd_src_no_ratelimit", "sd_src"];
-		foreach($qualities as $q){
-			if (isset($json[$q])) {
-				$filename = explode('/', $json[$q])[6];
-				$filename = explode('?', $filename)[0];
+		foreach ($qualities as $q) {
+			if (preg_match("@{$q}\":\"(https?://[^\"'><\r\n\t ]+)@i", $data, $link)) {
+				$link = $link[1];
+				$filename = substr($link, strrpos($link, '/') + 1);
+				$filename = explode('?', $filename);
+				$filename = $filename[0];
 				$this->lib->reserved['filename'] = $filename;
-				return $json[$q];
+				return $link;
 			}
 		}
 		$this->error("dead", true, false, 2);
@@ -28,5 +31,6 @@ class dl_facebook_com extends Download {
 * Facebook Video Download Plugin
 * Downloader Class By hogeunk
 * Made by hogeunk [2016/03/07]
+* Fix to support old videos by hogeunk [2016/03/10]
 */
 ?>
